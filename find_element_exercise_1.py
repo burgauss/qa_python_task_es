c868ae66f5881b0d9184d8832353bb5d4b78f99d
Juan Burgos <burgos.135@gmail.com>
2025-04-20 15:07:30 -0600

    first commit

---
+++ find_element_exercise_1.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
+
+# Buscar el título
+driver...
+
+# Cerrar el navegador
+driver...
---
+++ find_element_exercise_2.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
+
+# Encontrar todos los elementos
+elements = driver...
+
+# Comprobar que el número de elementos encontrados es mayor que 1 usando len()
+...
+
+# Cerrar el navegador
+driver...
