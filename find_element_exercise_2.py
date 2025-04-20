1e2e34a2fd6e34eebd530731bf796bef1bd4b979
Juan Burgos <burgos.135@gmail.com>
2025-04-20 15:52:42 -0600

    finish the exc2

--- find_element_exercise_2.py
+++ find_element_exercise_2.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -5,11 +6,13 @@ driver = webdriver.Chrome()
 
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
 
+time.sleep(3)
+
 # Encontrar todos los elementos
-elements = driver...
+elements = driver.find_elements(By.XPATH, ".//img")
 
 # Comprobar que el número de elementos encontrados es mayor que 1 usando len()
-...
+assert len(elements) > 1, f"Expected more than 1 <img> element, but found {len(elements)}"
 
 # Cerrar el navegador
-driver...
+driver.quit()
