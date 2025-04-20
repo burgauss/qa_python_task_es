1998c89b9cbe19dd55af19a5fb3d0971c3635edb
Juan Burgos <burgos.135@gmail.com>
2025-04-20 15:52:38 -0600

    finish the exc1

--- find_element_exercise_1.py
+++ find_element_exercise_1.py
@@ -1,12 +1,16 @@
-from selenium.webdriver.common.by import By
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
+driver.get('https://around-v1.nm.tripleten-services.com/signin?lng=es')
 
+time.sleep(5)
 # Buscar el título
-driver...
+title_element = driver.find_element(By.CSS_SELECTOR, ".auth-form__title")
+print(title_element.text)
 
 # Cerrar el navegador
-driver...
+driver.quit()
