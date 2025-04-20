62f1d4f55feae93b2e48bdb0f559685ac3429281
Juan Burgos <burgos.135@gmail.com>
2025-04-20 16:07:40 -0600

    finish the exc3

--- find_element_exercise_3.py
+++ find_element_exercise_3.py
@@ -1,17 +1,20 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
+time.sleep(3)
 
 # Buscar elementos
-email = driver...
-password = driver...
+email = driver.find_element(By.CSS_SELECTOR, "#email")
+password = driver.find_element(By.CSS_SELECTOR, "#password")
 
 # Probar el atributo placeholder para cada elemento
-assert ...
-assert ...
+assert email.get_attribute('placeholder') == "Correo electrónico"
+assert password.get_attribute('placeholder') == "Contraseña"
 
 # Cerrar el navegador
-driver...
+driver.quit()
